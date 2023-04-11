056c1d536e20c47502ce078f6aea45360fc57127
‘bingzhe’ <874177815@qq.com>
2023-04-11 11:26:46 +0800

    07shader

--- packages/07-threejs-shader/src/main/main01.js
+++ packages/07-threejs-shader/src/main/main01.js
@@ -5,9 +5,9 @@ import gsap from "gsap";
 import * as dat from "dat.gui";
 
 // 顶点着色器
-import basicVertexShader from "../shader/raw/vertex.glsl";
+import basicVertexShader from "../shader/basic/vertex.glsl";
 // 片元着色器
-import basicFragmentShader from "../shader/raw/fragment.glsl";
+import basicFragmentShader from "../shader/basic/fragment.glsl";
 
 // 目标：认识shader
 
@@ -42,36 +42,38 @@ scene.add(axesHelper);
 
 // 创建纹理加载器对象
 const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load("./texture/ca.jpeg");
+const texture = textureLoader.load("./texture/da.jpeg");
 const params = {
   uFrequency: 10,
   uScale: 0.1,
 };
 
 // const material = new THREE.MeshBasicMaterial({ color: "#00ff00" });
-// 创建原始着色器材质
-const rawShaderMaterial = new THREE.RawShaderMaterial({
+// 创建着色器材质
+// const shaderMaterial = new THREE.ShaderMaterial({
+//   vertexShader: `
+//         void main(){
+//             gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position, 1.0 ) ;
+//         }
+//     `,
+//   fragmentShader: `
+//         void main(){
+//             gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+//         }
+//   `,
+// });
+
+const shaderMaterial = new THREE.ShaderMaterial({
   vertexShader: basicVertexShader,
   fragmentShader: basicFragmentShader,
-  //   wireframe: true,
-  side: THREE.DoubleSide,
-  uniforms: {
-    uTime: {
-      value: 0,
-    },
-    uTexture: {
-      value: texture,
-    },
-  },
 });
 
 // 创建平面
 const floor = new THREE.Mesh(
   new THREE.PlaneBufferGeometry(1, 1, 64, 64),
-  rawShaderMaterial
+  shaderMaterial
 );
 
-console.log(floor);
 scene.add(floor);
 
 // 初始化渲染器
@@ -111,7 +113,6 @@ const clock = new THREE.Clock();
 function animate(t) {
   const elapsedTime = clock.getElapsedTime();
   //   console.log(elapsedTime);
-  rawShaderMaterial.uniforms.uTime.value = elapsedTime;
   requestAnimationFrame(animate);
   // 使用渲染器渲染相机看这个场景的内容渲染出来
   renderer.render(scene, camera);
